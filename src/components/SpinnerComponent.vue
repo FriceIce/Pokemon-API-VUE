cf7028796b29b7289bf8f75997ed34183808bff4
Isak Tilahun <tilahun.isak@gmail.com>
2024-12-03 17:59:59 +0100

    Created a loader component to indicate when the application is fetching data. For consistency, the loader uses the same icon displayed in the browser window for the application.

---
+++ src/components/SpinnerComponent.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section class="h-full grid place-items-center">
+    <img src="/pokemon-ball.svg" alt="pokemon ball" class="loader size-8 lg:size-10 mt-[-150px]" />
+  </section>
+</template>
+
+<style>
+.loader {
+  animation: spinner linear infinite;
+  animation-duration: 0.5s;
+}
+
+@keyframes spinner {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
